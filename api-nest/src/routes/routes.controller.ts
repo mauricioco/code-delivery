3f9bb1b21dd25c46e1446ba6eb5c9eb899e890ed
Mauricio <mauricio.carvoliveira@gmail.com>
2022-06-23 00:48:25 -0300

    Route model definition and basic GET methods.

--- api-nest/src/routes/routes.controller.ts
+++ api-nest/src/routes/routes.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
 
-@Controller('routes')
+@Controller('api/routes')
 export class RoutesController {
   constructor(private readonly routesService: RoutesService) {}
 
