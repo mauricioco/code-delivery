93df8520849760b55d252f257ddeb171f8cb7efa
Mauricio <mauricio.carvoliveira@gmail.com>
2022-06-24 00:14:03 -0300

    Integration with api. Also, added cors=true option to nest api creation.

--- client-react/src/components/Mapping.tsx
+++ client-react/src/components/Mapping.tsx
@@ -1,21 +1,39 @@
-// @flow 
 import { Button, Grid, MenuItem, Select } from '@material-ui/core';
-import * as React from 'react';
-type Props = {
-  
-};
+import { useEffect, useState } from 'react';
+import { Route } from '../util/models';
+
+const EMPTY_STRING = "";
+const API_URL = process.env.REACT_APP_API_URL;
+
+type Props = {};
 export const Mapping = (props: Props) => {
+  const [routes, setRoutes] = useState<Route[]>([]);
+  const [routeIdSelected, setRouteIdSelected] = useState<string>(EMPTY_STRING);
+
+  useEffect(() => {
+    fetch(`${API_URL}/routes`)
+      .then(data => data.json())
+      .then(data => setRoutes(data));
+  }, []);
+
   return (
     <Grid container>
       <Grid item xs={12} sm={3}>
         <form>
-          <Select fullWidth>
+          <Select 
+            fullWidth
+            displayEmpty
+            value={routeIdSelected} 
+            onChange={(event) => setRouteIdSelected(event.target.value as string)}
+          >
             <MenuItem value="">
               <em>Selecione uma corrida</em>
             </MenuItem>
-            <MenuItem value="1">
-              <em>Primeiro</em>
-            </MenuItem>
+            {routes.map((route, key) => (
+              <MenuItem key={key} value={route.id}>
+                {route.title}
+              </MenuItem>
+            ))}
           </Select>
           <Button type="submit" color="primary" variant="contained">Iniciar uma corrida</Button>
         </form>
