51041bbeeb8a97522bd18f4c2c489278f119f9cb
Mauricio <mauricio.carvoliveira@gmail.com>
2022-06-23 23:25:05 -0300

    Added Material UI dependency. Also fixed file watching on docker for windows with an env variable.

---
+++ client-react/src/components/Mapping.tsx
@@ -0,0 +1,14 @@
+// @flow 
+import { Grid } from '@material-ui/core';
+import * as React from 'react';
+type Props = {
+  
+};
+export const Mapping = (props: Props) => {
+  return (
+    <div>
+      <Grid item>Formulário</Grid>
+      <Grid item>Map</Grid>
+    </div>
+  );
+};
